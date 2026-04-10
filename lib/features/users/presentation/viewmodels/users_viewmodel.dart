6e5428508d21f33dafc55f68610550e10e174d14
e3rosale <eduardo.d.rosales1@gmail.com>
2026-04-09 18:07:11 -0700

    fix: added import statement for UserEntity in UsersViewModel

--- lib/features/users/presentation/viewmodels/users_viewmodel.dart
+++ lib/features/users/presentation/viewmodels/users_viewmodel.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/foundation.dart';
 
 import '../../../../core/utils/result.dart';
-// import '../../domain/entities/user_entity.dart';
-import '../../domain/usecases/create_user.dart';
-import '../../domain/usecases/delete_user.dart';
-import '../../domain/usecases/get_users.dart';
-import '../../domain/usecases/update_user.dart';
+import '../../domain/entities/user_entity.dart';
+// import '../../domain/usecases/create_user.dart';
+// import '../../domain/usecases/delete_user.dart';
+// import '../../domain/usecases/get_users.dart';
+// import '../../domain/usecases/update_user.dart';
 
 class UsersViewModel extends ChangeNotifier {
   final GetUsers getUsersUseCase;
