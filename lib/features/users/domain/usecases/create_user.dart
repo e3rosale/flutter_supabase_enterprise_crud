2116358121efcae76163215d7c0c6b10a91f44d9
e3rosale <eduardo.d.rosales1@gmail.com>
2026-04-09 21:15:44 -0700

    fix: added implementation for CreateUser

---
+++ lib/features/users/domain/usecases/create_user.dart
@@ -0,0 +1,12 @@
+import '../../../../core/utils/result.dart';
+// import '../repositories/user_repository.dart';
+
+class CreateUser {
+  final UserRepository repository;
+
+  CreateUser(this.repository);
+
+  Future<Result<void>> call({required String name, required String email}) {
+    return repository.createUser(name: name, email: email);
+  }
+}
